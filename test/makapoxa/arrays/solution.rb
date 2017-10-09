74254e89534a1a0366b599d0d553f82e3acfb30f
Petr Ablyazov <phoenix-h@mail.ru>
2017-10-09 12:39:29 +0300

    Array.Makapoxa min_repeat - done

--- test/makapoxa/arrays/solution.rb
+++ test/makapoxa/arrays/solution.rb
@@ -2,7 +2,20 @@ module Makapoxa
   module Arrays
     class << self
       def min_repeat(array)
-        array
+        arr_res = {}
+        interval_val = ''
+        i = 0
+        array.each_with_index do |el|
+          if el == interval_val
+            arr_res[i] += 1
+          else
+            i += 1
+            arr_res[i] = 1
+          end
+          interval_val = el         
+        end
+        arr_res.delete_if {|k,v| v < 2} 
+        arr_res.empty? ? 0 : arr_res.values.min      
       end
 
       def search(array, query)
