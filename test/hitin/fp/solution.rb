ef5974b61eacdfe206cbae256c1fee8bc747f552
Petr Ablyazov <phoenix-h@mail.ru>
2017-10-13 13:49:15 +0300

    fp rating - done

--- test/hitin/fp/solution.rb
+++ test/hitin/fp/solution.rb
@@ -5,11 +5,14 @@ module Hitin
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array, access_level)
-
+        rating = []
+        array.each { |film| rating << film['rating_kinopoisk'].to_f if film['access_level'] == access_level && film['rating_kinopoisk'].to_f.positive? }
+        rating.reduce(:+) / rating.length
       end
 
       def chars_count(films, year)
-
+        chars = []
+        films.each { |film| chars << film['name'] if film['year'] == year.to_s }
       end
     end
   end
