cb4116c0c733fba82891497fedf1287e87969b9f
Petr Ablyazov <phoenix-h@mail.ru>
2017-10-17 08:19:10 +0300

    fp2 -done

--- test/hitin/fp2/solution.rb
+++ test/hitin/fp2/solution.rb
@@ -4,13 +4,31 @@ module Hitin
       # Использовать стандартные функции массива для решения задач нельзя.
       # Использовать свои написанные функции для реализации следующих - можно.
       # Написать свою функцию my_each
-      # def my_each end
+      def my_each
+        for i in self
+          yield(i)
+        end
+      end
+
       # Написать свою функцию my_map
-      # def my_map end
+      def my_map
+        result = MyArray.new([])
+        my_each { |i| result << yield(i) }
+        result
+      end
+
       # Написать свою функцию my_compact
-      # def my_compact end
+      def my_compact
+        result = MyArray.new([])
+        my_each { |i| result << i if i }
+        result
+      end
+
       # Написать свою функцию my_reduce
-      # def my_reduce end
+      def my_reduce(sum = nil)
+        my_each { |i| sum = sum ? yield(i, sum) : i }
+        sum
+      end
     end
   end
 end
